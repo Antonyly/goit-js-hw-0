a2cd0f14f4407ddd81c7486ee9cf14fef7eee2cc
Antonyly <antonlyzun@gmail.com>
2022-07-05 19:59:22 +0300

    js

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+// const allCategoriesEl = document.querySelectorAll('.item');
+// console.log(`Number of categories:  ${allCategoriesEl.length}`);
+
+// const categoriesEl = document.querySelectorAll('#categories > li');
+// categoriesEl.forEach(el => {
+//     console.log(`Category: ${el.children[0].textContent}
+// Elements: ${el.lastElementChild.children.length}`);
+// });
+
+const allCategoriesEl = document.querySelectorAll('.item');
+console.log(`Number of categories:  ${allCategoriesEl.length}`);
+
+const categoriesEl = document.querySelectorAll('#categories > li');
+categoriesEl.forEach(el => {
+    console.log(`Category: ${el.children[0].textContent}
+Elements: ${el.children[1].children.length}`);
+});
