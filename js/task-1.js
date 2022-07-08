cdfbc13bc028bf421083e220a84d3893d6bc7a31
Antonyly <antonlyzun@gmail.com>
2022-07-08 06:53:36 +0300

    +

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,3 @@
-// const allCategoriesEl = document.querySelectorAll('.item');
-// console.log(`Number of categories:  ${allCategoriesEl.length}`);
-
-// const categoriesEl = document.querySelectorAll('#categories > li');
-// categoriesEl.forEach(el => {
-//     console.log(`Category: ${el.children[0].textContent}
-// Elements: ${el.lastElementChild.children.length}`);
-// });
-
 const allCategoriesEl = document.querySelectorAll('.item');
 console.log(`Number of categories:  ${allCategoriesEl.length}`);
 
--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,16 @@
-
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const itemsListEl = document.querySelector('#ingredients')
+const makeItemsEl = ingredients.map(el => {
+    let addEl = document.createElement('li');
+    addEl.textContent = el;
+    return addEl;
+});
+itemsListEl.append(...makeItemsEl);
+console.log(itemsListEl);
